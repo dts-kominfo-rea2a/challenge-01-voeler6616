ccff5c6d79937598890562862309ae13b450c15f
voeler6616 <rvoeler6616@gmail.com>
2022-05-27 08:37:26 +0700

    Add object to users variable

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+// First User Object
+const firstUser = {
+    name            : 'Monica',
+    gender          : 'Female',
+    age             : 17,
+    email           : 'monica@dingdong.com',
+    favoritColor    : new Set(['Yellow', 'Pink', 'White', 'Purple']),
+    isHavePet       : 'Yes',
+    education       :
+    {
+        name    : 'SD 01',
+        city    : 'Jakarta',
+        graduate: 2016
+    },
+    favoriteRestaurant: new Set([
+        "Bento",
+        "Sushi",
+        "Pancake",
+        "Eggy",
+        "Tempura",
+        "Bento",
+        "Eggy",
+        "Padang",
+        "Tteok",
+        "Sushi",
+        "Sushi",
+    ]),
+};
+
+// Second User Object
+const secondUser = {
+    name: 'Wendy',
+    gender: 'Male',
+    age: 23,
+    email: 'wendy@dingdong.com',
+    favoritColor: new Set(['Blue', 'Black', 'Grey']),
+    isHavePet: 'No',
+    education:
+    {
+        name: 'SD 02',
+        city: 'Jakarta',
+        graduate: 2010
+    },
+    favoriteRestaurant: new Set([
+        "Tempura",
+        "Bento",
+        "Sushi",
+        "Pancake",
+        "Padang",
+        "Katsu",
+        "Geprek",
+        "Pancake",
+        "Eggy",
+    ]),
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+
+users.push(firstUser, secondUser);
+
+console.log(users);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
